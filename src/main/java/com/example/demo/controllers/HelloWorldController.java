2823db85da1fe122fd085b7575b3ccb9affe1913
The Overlord <james.decker@libertymutual.com>
2017-08-10 16:02:34 -0700

    added get functionality

--- src/main/java/com/example/demo/controllers/HelloWorldController.java
+++ src/main/java/com/example/demo/controllers/HelloWorldController.java
@@ -1,15 +1,11 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.demo.models.Adder;
-import com.example.demo.models.Subtractor;
 import com.example.demo.models.Whisperer;
 import com.example.demo.models.Yeller;
 
@@ -27,12 +23,6 @@ public class HelloWorldController {
 	public String index() {
 		return "helloworld/index";
 	}
-	
-	// create a class named Whisperer that makes Strings lower case
-	// Using the method below, create a method named "whisper" for a GetMapping that
-	//   does essentially the same thing *BUT* uses Whisperer.
-	// This means that you should map a GET to the path "whisper"
-	// This means that you should have a method named "whisper" that returns a ModelAndView
 	
 	@GetMapping("message")
 	public ModelAndView message(@RequestParam(required=false, defaultValue="«silence»") String message) {
@@ -54,20 +44,5 @@ public class HelloWorldController {
 		return mv;
 	}
 	 
-	@PostMapping("adder")
-	public String addTwoNumbers(@RequestParam(name="left") int first, @RequestParam(name="right") double second, Model model) {
-		Adder adder = new Adder(first, second);
-		double result = adder.calculate();
-		model.addAttribute("sum", result);
-		return "helloworld/sum-result";
-	}
-	
-	@PostMapping("subtractor")
-	public String subtractTwoNumbers(@RequestParam(name="left") int first, @RequestParam(name="right") double second, Model model) {
-		Subtractor subtractor = new Subtractor(first, second);
-		double result = subtractor.calculate();
-		model.addAttribute("difference", result);
-		return "helloworld/difference-result";
-	}
 	
 }
---
+++ src/main/java/com/example/demo/controllers/MathController.java
@@ -0,0 +1,51 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.models.Adder;
+import com.example.demo.models.Subtractor;
+
+@Controller
+@RequestMapping("/math")
+public class MathController {
+
+	@PostMapping("adder")
+	public String addTwoNumbers(@RequestParam(name="left") int first, @RequestParam(name="right") double second, Model model) {
+		Adder adder = new Adder(first, second);
+		double result = adder.calculate();
+		model.addAttribute("sum", result);
+		return "helloworld/sum-result";
+	}
+	
+	// homework
+	@GetMapping("adder")
+	public String addTwoNumbersGet(Model model) {
+		Adder adder = new Adder(0, 0);
+		double result = adder.calculate();
+		model.addAttribute("sum", result);
+		return "helloworld/sum-result";
+	}
+	
+	@PostMapping("subtractor")
+	public String subtractTwoNumbers(@RequestParam(name="left") int first, @RequestParam(name="right") double second, Model model) {
+		Subtractor subtractor = new Subtractor(first, second);
+		double result = subtractor.calculate();
+		model.addAttribute("difference", result);
+		return "helloworld/difference-result";
+	}
+	
+	// homework
+	@GetMapping("subtractor")
+	public String subtractTwoNumbersGet(Model model) {
+		Subtractor subtractor = new Subtractor(0, 0);
+		double result = subtractor.calculate();
+		model.addAttribute("difference", result);
+		return "helloworld/difference-result";
+	}
+		
+}
